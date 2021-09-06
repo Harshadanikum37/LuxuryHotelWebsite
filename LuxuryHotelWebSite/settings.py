fe99619b16c0c37541f0fcb779b2f68b4d8804f4
harishdeore10 <harishdeore10@gmail.com>
2021-09-06 20:43:50 +0530

    Updated password

--- LuxuryHotelWebSite/settings.py
+++ LuxuryHotelWebSite/settings.py
@@ -134,6 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'harishdeore10@gmail.com'
-EMAIL_HOST_PASSWORD = '<password>'
+EMAIL_HOST_PASSWORD = 'London@2050'
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = False
