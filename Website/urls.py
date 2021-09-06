282dbbdf8d4f1aad775033888455c1041092af3a
harishdeore10 <harishdeore10@gmail.com>
2021-09-06 17:30:22 +0530

    Added html files

--- Website/urls.py
+++ Website/urls.py
@@ -20,6 +20,7 @@ from Website import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('index.html', views.home, name='home'),
     path('contact.html', views.contact, name='contact'),
     path('blog.html', views.blog, name='blog'),
     path('about.html', views.about, name='about'),
